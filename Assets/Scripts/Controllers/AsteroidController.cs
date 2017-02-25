37c5b5ee102aeeabb218db9972e9f9c0602294e1
Eregrith <eregrith@gmail.com>
2017-02-25 12:55:59 +0100

    Ajout d'un asteroid de copper, ajout d'un spawner d'asteroid, suppression du spawn de ressources jusqu'a trouver un meilleur moyen

--- Assets/Scripts/Controllers/AsteroidController.cs
+++ Assets/Scripts/Controllers/AsteroidController.cs
@@ -3,33 +3,33 @@
 public class AsteroidController : MonoBehaviour {
 
     private float RotationSpeed { get; set; }
-    private int SpriteIndex { get; set; }
+    private Vector3 Movement { get; set; }
     public Sprite[] Sprites;
     public GameObject PickupSpawned;
     public int MinimumSpawn;
     public int MaximumSpawn;
 
     void Start () {
-        RotationSpeed = Random.Range(-3f, 3f);
+        RotationSpeed = Random.Range(-90f, 90f);
         transform.Rotate(0, 0, 500*RotationSpeed);
         GetComponent<SpriteRenderer>().sprite = Sprites[Random.Range(0, Sprites.Length)];
+        Movement = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
     }
 	
-	// Update is called once per frame
-	void Update () {
-        transform.Rotate(0, 0, RotationSpeed);
+	void Update ()
+    {
+        transform.Translate(Movement.x * Time.deltaTime, Movement.y * Time.deltaTime, 0, Space.World);
+        transform.Rotate(Vector3.forward, RotationSpeed * Time.deltaTime, Space.Self);
 	}
 
     private void OnDestroy()
     {
-        SpawnResources();
+        //SpawnResources();
     }
 
     private void SpawnResources()
     {
-        Debug.Log("Spawning resources !");
         int spawned = Random.Range(MinimumSpawn, MaximumSpawn);
-        Debug.Log("Spawning " + spawned);
         while (spawned > 0)
         {
             Instantiate(PickupSpawned, this.transform.position, new Quaternion());
---
+++ Assets/Scripts/Controllers/AsteroidSpawnerController.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Scripts.Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawnerController : MonoBehaviour {
+
+    public AsteroidSpawnable[] Asteroids;
+    public float SpawnInterval;
+    private float SpawnTimer;
+
+    private void SpawnAsteroid(AsteroidSpawnable spawnable)
+    {
+        Instantiate(spawnable.Asteroid, new Vector3(), Quaternion.identity);
+    }
+
+    private void TrySpawningAsteroids()
+    {
+        foreach (AsteroidSpawnable spawnable in Asteroids)
+            if (Random.value <= spawnable.SpawnChance)
+                SpawnAsteroid(spawnable);
+    }
+
+    public void Start()
+    {
+        SpawnTimer = SpawnInterval;
+    }
+
+    public void Update()
+    {
+        SpawnTimer -= Time.deltaTime;
+        if (SpawnTimer <= 0)
+        {
+            SpawnTimer = SpawnInterval;
+            TrySpawningAsteroids();
+        }
+    }
+}
